3ad02227cfcb32cca1a2dfb507ffd9d464793a17
YoruanOrta <yoruanorta50@gmail.com>
2025-10-03 01:01:19 -0400

    NOW IS READY FOR DEPLOYYYYYYYYYY

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 // Change this URL after you deploy to Render
-const PRODUCTION_URL = 'your-app-name.onrender.com';
+const PRODUCTION_URL = 'chat-app-7kr1.onrender.com';
 
 export const config = {
   WS_URL: isProduction 
